9a1b75d2623a62162bb2abdc5a3b5596505f1060
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-07-07 00:26:56 -0500

    update @rollup/babel

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 // import typescript from "@rollup/plugin-typescript";
 // import coffeescript from 'rollup-plugin-coffee-script';
-import babel from "rollup-plugin-babel";
+import { babel } from '@rollup/plugin-babel';
 // import json from "@rollup/plugin-json"
 import { terser } from "rollup-plugin-terser";
 import autoExternal from 'rollup-plugin-auto-external';
@@ -27,7 +27,7 @@ let plugins = [
   //   { compact: true }
   // ),
 
-  babel(),
+  babel({ extensions: ['.js', '.coffee'], babelHelpers: 'bundled' }),
 
   // so Rollup can find externals
   resolve({ extensions: ["ts", ".js", ".coffee"], preferBuiltins: true }),
