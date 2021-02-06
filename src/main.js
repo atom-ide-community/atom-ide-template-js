8d4e453b9a154a2471be81440e99187d953c0a6b
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-05 23:03:17 -0600

    fix: update code to match atom-ide-template

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
 import { CompositeDisposable } from "atom"
 
-let subscriptions
+// Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
+const subscriptions = new CompositeDisposable()
 
 /**
  * called by Atom when activating an extension
- * @param  {any} state the current state of atom
  */
-export function activate(state) {
-  // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
-  subscriptions = new CompositeDisposable()
-
+export function activate() {
   package_deps().then(() => {
     // do package stuff here
-    atom.notifications.addInfo("Hi")
   })
 }
 
@@ -22,7 +18,7 @@ export function activate(state) {
 async function package_deps() {
   // Add entries from package-deps here manually
   // (to prevent loading atom-package-deps and package.json when the deps are already loaded)
-  const deps = []
+  const deps: string[] = []
   if (deps.some((p) => !atom.packages.isPackageLoaded(p))) {
     await import("atom-package-deps").then((atom_package_deps) => {
       // install if not installed
@@ -45,14 +41,15 @@ export function deactivate() {
   if (subscriptions) {
     subscriptions.dispose()
   }
-  subscriptions = null
 }
 
 /**
  * called by IDE extensions to retrieve the service for registration
  * @return the current instance
  */
-export function provideYourService() {}
+export function provideYourService() {
+  // your service
+}
 
 export const config = {
   doSomething: {
